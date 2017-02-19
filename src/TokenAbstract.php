e1676d8909bdb28852e3e601be306efea560f6bd
Rob Waller <rdwaller1984@gmail.com>
2017-02-19 18:52:08 +0000

    Added comments to the Token Abstract class

--- src/TokenAbstract.php
+++ src/TokenAbstract.php
@@ -1,16 +1,40 @@
 <?php namespace ReallySimpleJWT;
 
+/**
+ * Abstract Token class that contains some global features for the Token Builder
+ * class and the Token Validator class
+ */
 abstract class TokenAbstract 
 {
+	/**
+	 * The hash type for the signature hashing 
+	 *
+	 * @var string
+	 */
 	private $hash = 'sha256';
 
+	/**
+	 * The JWT hash algorithm type for the Token Header
+	 *
+	 * @var string
+	 */
 	private $algorithm = 'HS256';
 
+	/**
+	 * Return the hash type for the signature hashing
+	 *
+	 * @return string
+	 */
 	public function getHash()
 	{
 		return $this->hash;
 	}
 
+	/**
+	 * Return the JWT hash algorithm type for the Token header
+	 *
+	 * @return string
+	 */
 	public function getAlgorithm()
 	{
 		return $this->algorithm;
