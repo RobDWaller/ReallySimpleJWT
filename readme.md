fc85a91d696cdcd7818905e0cda0ce20e23adf55
Rob Waller <rdwaller1984@gmail.com>
2017-02-19 16:20:30 +0000

    Updated readme and added some comments to Token class

--- readme.md
+++ readme.md
@@ -1,1 +1,36 @@
 # Really Simple JSON Web Tokens
+
+A simple Package for creating JSON Web Tokens that uses HMAC SHA256 to sign
+signatures. Exposes a simple interface to allow you to create a simple token
+that stores a user identifier. The Package is set up to allow extension and
+the use of larger payloads.
+
+For more information on JSON Web Tokens please see https://jwt.io
+
+## Usage
+
+### Get Token
+
+```
+<?php
+
+use ReallySimpleJWT\Token;
+
+$token = Token::getToken(1, 'secret', '2017-01-01 01:01:01', '127.0.0.1');
+```
+
+### Validate Token
+
+```
+<?php
+
+use ReallySimpleJWT\Token;
+
+$result = Token::validate('token', 'secret');
+```
+
+## Author
+
+Rob Waller
+Email: rdwaller1984@gmail.com
+Twitter: @robdwaller 
--- src/Token.php
+++ src/Token.php
@@ -1,7 +1,28 @@
 <?php namespace ReallySimpleJWT;
 
+/**
+ * A simple Package for creating JSON Web Tokens that uses HMAC SHA256 to sign
+ * signatures. Exposes a simple interface to allow you to create a simple token
+ * that stores a user identifier. The Package is set up to allow extension and
+ * the use of larger payloads.
+ *
+ * For more information on JSON Web Tokens please see https://jwt.io 
+ * 
+ * @author Rob Waller <rdwaller1984@gmail.com>
+ */
+
 class Token
 {
+	/**
+	 * Create a JSON Web Token that contains a User Identifier Payload 
+	 *
+	 * @param string/int $userId
+	 * @param string/int $secret
+	 * @param datetimestring $expiration
+	 * @param string $issuer
+	 *
+	 * @return string
+	 */
 	public static function getToken($userId, $secret, $expiration, $issuer)
 	{
 		$builder = Self::builder();
@@ -13,6 +34,14 @@ class Token
 			->build();
 	}
 
+	/**
+	 * Validate a JSON Web Token's expiration and signature  
+	 *
+	 * @param string $token
+	 * @param string/int $secret
+	 *
+	 * @return bool
+	 */
 	public static function validate($token, $secret)
 	{
 		$validator = Self::validator();
@@ -22,11 +51,21 @@ class Token
 			->validateSignature($secret);
 	}
 
+	/**
+	 * Interface to return instance of the token builder
+	 *
+	 * @return TokenBuilder 
+	 */
 	public static function builder()
 	{
 		return new TokenBuilder();
 	}
 
+	/**
+	 * Interface to return instance of the token validator
+	 *
+	 * @return TokenValidator 
+	 */
 	public static function validator()
 	{
 		return new TokenValidator();
