05908426abea8e4c4f3f00721cccbc80a2ed9f69
RobDWaller <rdwaller1984@googlemail.com>
2018-11-20 21:23:21 +0000

    Added extra token structure tests including invalid test to validate class.

--- tests/ValidateTest.php
+++ tests/ValidateTest.php
@@ -3,9 +3,11 @@
 namespace Test;
 
 use ReflectionMethod;
+use PHPUnit\Framework\TestCase;
 use ReallySimpleJWT\Validate;
 use ReallySimpleJWT\Jwt;
-use PHPUnit\Framework\TestCase;
+use ReallySimpleJWT\Token;
+use Carbon\Carbon;
 
 class ValidateTest extends TestCase
 {
@@ -30,5 +32,33 @@ class ValidateTest extends TestCase
         $result = $method->invoke($validate);
 
         $this->assertTrue($result);
+    }
+
+    public function testValidateTokenStructureWithRSJWT()
+    {
+        $jwt = new Jwt(Token::getToken(1, 'foo1234He$$llo56', Carbon::now()->addMinutes(5)->toDateTimeString(), '127.0.0.1'));
+
+        $validate = new Validate($jwt);
+
+        $method = new ReflectionMethod(Validate::class, 'tokenStructure');
+        $method->setAccessible(true);
+
+        $result = $method->invoke($validate);
+
+        $this->assertTrue($result);
+    }
+
+    public function testValidateTokenStructureInvalid()
+    {
+        $jwt = new Jwt('eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9');
+
+        $validate = new Validate($jwt);
+
+        $method = new ReflectionMethod(Validate::class, 'tokenStructure');
+        $method->setAccessible(true);
+
+        $result = $method->invoke($validate);
+
+        $this->assertFalse($result);
     }
 }
