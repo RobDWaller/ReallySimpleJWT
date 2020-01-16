dd1880cfab9d1ec6d93f99627582b915001613a0
RobDWaller <rdwaller1984@googlemail.com>
2020-01-16 07:48:00 +0000

    Applied psr-12 codelint fixes to new Secret class and tests.

--- src/Secret.php
+++ src/Secret.php
@@ -8,13 +8,13 @@ use ReallySimpleJWT\Interfaces\Secret as SecretInterface;
 
 /**
  * Validate the secret used to secure the token signature is strong enough.
- * You can define your own secret validation by creating a new class and 
+ * You can define your own secret validation by creating a new class and
  * implementing the Secret interface.
  */
 class Secret implements SecretInterface
 {
     /**
-     * The secret should contain a number, a upper and a lowercase letter, and a 
+     * The secret should contain a number, a upper and a lowercase letter, and a
      * special character *&!@%^#$. It should be at least 12 characters in length.
      *
      * The regex here uses lookahead assertions.
