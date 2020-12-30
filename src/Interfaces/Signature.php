266d36d8ca4c373cf3be07a6b5f4241f315de5cd
RobDWaller <rdwaller1984@gmail.com>
2020-12-30 16:01:13 +0000

    Added types and doc types to Signature interface.

--- src/Interfaces/Signature.php
+++ src/Interfaces/Signature.php
@@ -4,5 +4,9 @@ namespace ReallySimpleJWT\Interfaces;
 
 interface Signature
 {
+    /**
+     * @param mixed[] $header
+     * @param mixed[] $payload 
+     */
     public function make(array $header, array $payload, string $secret): string;
 }
