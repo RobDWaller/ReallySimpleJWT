a6a0a2200feb6bf597b2b80fda0cab087f8dbfac
RobDWaller <rdwaller1984@gmail.com>
2020-12-30 15:46:50 +0000

    Applied type and doc type fixes to Encoders and Decoders.

--- src/Interfaces/Encode.php
+++ src/Interfaces/Encode.php
@@ -8,11 +8,16 @@ interface Encode
 
     /**
      * Encode a JSON string so it is base64URL compliant.
+     * 
+     * @param mixed[] $toEncode
      */
     public function encode(array $toEncode): string;
 
     /**
      * Create the JSON Web Token signature string.
+     * 
+     * @param mixed[] $header
+     * @param mixed[] $payload
      */
     public function signature(array $header, array $payload, string $secret): string;
 }
