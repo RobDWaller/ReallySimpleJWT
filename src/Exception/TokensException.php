62ac91a32aaa19d6e3e72a2f4b73250a1b0163f5
Rob Waller <rdwaller1984@gmail.com>
2021-01-16 15:56:31 +0000

    Created Tokens Exception class.

---
+++ src/Exception/TokensException.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace ReallySimpleJWT\Exception;
+
+use Exception;
+
+/**
+ * Simple PHP exception class for all tokens exceptions so exceptions are
+ * more specific and obvious.
+ */
+class TokensException extends Exception
+{
+}
