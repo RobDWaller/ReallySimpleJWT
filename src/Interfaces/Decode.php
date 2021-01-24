542a26231fe5fabfe7db84e191a2ce19eab2994b
Rob Waller <rdwaller1984@gmail.com>
2021-01-24 15:28:48 +0000

    Updated Decode Interface comments.

--- src/Interfaces/Decode.php
+++ src/Interfaces/Decode.php
@@ -2,11 +2,15 @@
 
 namespace ReallySimpleJWT\Interfaces;
 
+/**
+ * Interface for Decode class if customisation is required.
+ */
 interface Decode
 {
     /**
-     * Decode a base64URL string to a JSON string.
+     * Decode a base64URL string to an associative array.
      *
+     * @see Decode::decode()
      * @return mixed[]
      */
     public function decode(string $toDecode): array;
