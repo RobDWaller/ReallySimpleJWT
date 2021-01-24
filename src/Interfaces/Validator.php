8ed80581ab63c4ab00670d10bcdc995e3cda9d66
Rob Waller <rdwaller1984@gmail.com>
2021-01-24 15:12:13 +0000

    Updated Validator interface comments.

--- src/Interfaces/Validator.php
+++ src/Interfaces/Validator.php
@@ -5,64 +5,51 @@ declare(strict_types=1);
 namespace ReallySimpleJWT\Interfaces;
 
 /**
- * A validation helper class which offers methods to confirm the validity of
- * a JSON Web Token along with aspects of its content.
- *
- * Error codes and messages:
- * 1: Token is invalid: Token must have three parts separated by dots.
- * 2: Audience claim does not contain provided StringOrURI.
- * 3: Signature is invalid: Signature does not match header / payload content.
- * 4: Expiration claim has expired.
- * 5: Not Before claim has not elapsed.
- * 6: Expiration claim is not set.
- * 7: Not Before claim is not set.
- * 8: Invalid payload claim: Claims must be key values of type string:mixed.
- * 9: Invalid secret: See README for more information.
- * 10: Invalid Audience claim: Must be either a string or array of strings.
+ * Interface for Validator class to allow developers to implement custom token
+ * validation if required.
  */
 interface Validator
 {
     /**
-     * Confirm the structure of a JSON Web Token, it has three parts separated
-     * by dots and complies with Base64URL standards.
+     * Confirm the structure of a JSON Web Token.
+     *
+     * @see Helper\Validator::structure()
      */
     public function structure(string $jwt): bool;
 
     /**
-     * Check the validity of the JWT's expiration claim as defined in the
-     * token payload. Returns false if the expiration time has surpassed the
-     * current time.
+     * Check the validity of the JWT's expiration claim.
+     *
+     * @see Helper\Validator::expiration()
      */
     public function expiration(int $expiration): bool;
 
     /**
-     * Check the validity of the JWT's not before claim as defined in the
-     * token payload. Returns false if the not before time has not surpassed
-     * the current time.
+     * Check the validity of the JWT's not before claim.
+     *
+     * @see Helper\Validator::notBefore()
      */
     public function notBefore(int $notBefore): bool;
 
     /**
-     * Check the validity of the JWT's audience claim. The audience claim
-     * defines the recipient or recipients allowed to process the token. This
-     * claim can either be a StringOrURI or an array of StringOrURIs.
+     * Check the validity of the JWT's audience claim.
      *
+     * @see Helper\Validator::audience()
      * @param string|string[] $audience
      */
     public function audience($audience, string $check): bool;
 
     /**
-     * Check two signature hashes match. One signature is supplied by the token.
-     * The other is newly generated from the token's header and payload. They
-     * should match, if they don't someone has likely tampered with the token.
+     * Check the token signature and generated signature match.
+     *
+     * @see Helper\Validator::signature()
      */
     public function signature(string $generatedSignature, string $tokenSignature): bool;
 
     /**
-     * Check the alg claim is in the list of valid algorithms. These are the
-     * valid digital signatures, MAC algorithms or "none" as
-     * defined in RFC 7518.
+     * Check the alg claim is in the list of valid algorithms.
      *
+     * @see Helper\Validator::algorithm()
      * @param string[] $validAlgorithms
      */
     public function algorithm(string $algorithm, array $validAlgorithms): bool;
