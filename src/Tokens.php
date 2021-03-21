36fb90ce2a0d107fca817ab05a0ba896c748461e
Rob Waller <rdwaller1984@gmail.com>
2021-03-21 15:07:27 +0000

    Chained the validate methods in Tokens validate method.

--- src/Tokens.php
+++ src/Tokens.php
@@ -134,9 +134,9 @@ class Tokens
         $validate = $this->validator($token, $secret);
 
         try {
-            $validate->structure();
-            $validate->algorithmNotNone();
-            $validate->signature();
+            $validate->structure()
+                ->algorithmNotNone()
+                ->signature();
             return true;
         } catch (ValidateException $e) {
             return false;
