72a36501f431f10b7996bcc527dcb70f356ea6a2
Rob Waller <rdwaller1984@gmail.com>
2021-12-12 14:30:41 +0000

    Created an EncodeException class to highlight exceptions generated in Encode classes.

---
+++ src/Exception/EncodeException.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace ReallySimpleJWT\Exception;
+
+use Exception;
+
+/**
+ * Exception class for exceptions which occur in the Encode classes.
+ */
+class EncodeException extends Exception
+{
+}
