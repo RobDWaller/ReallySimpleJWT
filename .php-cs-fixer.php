dbb6e235ca27da201d80c0ac4cca23537189c21c
Rob Waller <rdwaller1984@gmail.com>
2021-12-14 15:12:24 +0000

    Added benchmarks directory to CS Fixer checks.

--- .php-cs-fixer.php
+++ .php-cs-fixer.php
@@ -1,7 +1,7 @@
 <?php
 
 $finder = PhpCsFixer\Finder::create()->in(
-    [__DIR__ . '/src', __DIR__ . '/tests']
+    [__DIR__ . '/src', __DIR__ . '/tests', __DIR__ . '/benchmarks']
 );
 
 $config = new PhpCsFixer\Config();
