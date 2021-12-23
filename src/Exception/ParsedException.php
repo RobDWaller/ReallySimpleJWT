c46f7c9fe4d96d7e2e2b67eb5a9ca482ae6926f7
Rob Waller <rdwaller1984@gmail.com>
2021-12-23 11:21:18 +0000

    Replaced ParseException with ParsedException as relevant functionality has moved from the Parse class to the Parsed class.

---
+++ src/Exception/ParsedException.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace ReallySimpleJWT\Exception;
+
+use Exception;
+
+/**
+ * Exception class for exceptions which occur in the Parsed class.
+ */
+class ParsedException extends Exception
+{
+}
